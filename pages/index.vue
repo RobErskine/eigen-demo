fcf88fb0a19dcd5e8254d5e2c236dc5a89cd7d8f
Rob Erskine <rob@roberskine.com>
2022-12-12 23:10:17 -0500

    working eigen demo

--- pages/index.vue
+++ pages/index.vue
@@ -1,55 +1,61 @@
 <template>
     <div>
-        <form id="PaymentForm" action="" method="POST">
-            <input type="hidden" name="InvoiceID" value="DEMO-12345" />
-            <input id="MPXSessionID" type="hidden" name="MPXSessionID" value="aabbccdd-0123-4567-89ef-aabbccddeeff" /> 
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Invoice ID:</th> 
-                        <td>DEMO-12345</td>
-                    </tr> 
-                    <tr>
-                        <th>Amount Due:</th>
-                        <td>$15.00</td> 
-                    </tr>
-                    <tr>
-                        <th>Cardholder Name:</th>
-                        <td>
-                            <input id="CardholderName" type="text" name="CardholderName" defaultValue="Test Person" />
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>Card Number:</th>
-                        <td id="CardNumberCell"></td> 
-                    </tr>
-                    <tr>
-                        <th>Expiry Date (MMYY):</th>
-                        <td id="ExpiryDateCell"></td> 
-                    </tr>
-                    <tr>
-                        <th>CVV:</th>
-                        <td id="CVVCell"></td> 
-                    </tr>
-                    <tr>
-                        <th>Postal Code:</th>
-                        <td>
-                            <input type="text" name="PostalCode" defaultValue="h0h 0h0" />
-                        </td>    
-                    </tr>
-                    <tr>
-                        <td colSpan={2}>
-                            <button type="button" id="ProcessButton">Process Payment</button>
-                        </td>
-                    </tr>
-                </tbody> 
-            </table>
-        </form>
+        <div v-if="pending">
+            Attempting to start MiraPay Capture...
+        </div>
+        <div v-else>
+            <form id="PaymentForm" action="" method="POST">
+                <input type="hidden" name="InvoiceID" value="DEMO-12345" />
+                <!-- <input id="MPXSessionID" type="hidden" name="MPXSessionID" value="aabbccdd-0123-4567-89ef-aabbccddeeff" />  -->
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>Invoice ID:</th> 
+                            <td>DEMO-12345</td>
+                        </tr> 
+                        <tr>
+                            <th>Amount Due:</th>
+                            <td>$15.00</td> 
+                        </tr>
+                        <tr>
+                            <th>Cardholder Name:</th>
+                            <td>
+                                <input id="CardholderName" type="text" name="CardholderName" defaultValue="Test Person" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>Card Number:</th>
+                            <td id="CardNumberCell"></td> 
+                        </tr>
+                        <tr>
+                            <th>Expiry Date (MMYY):</th>
+                            <td id="ExpiryDateCell"></td> 
+                        </tr>
+                        <tr>
+                            <th>CVV:</th>
+                            <td id="CVVCell"></td> 
+                        </tr>
+                        <tr>
+                            <th>Postal Code:</th>
+                            <td>
+                                <input type="text" name="PostalCode" defaultValue="h0h 0h0" />
+                            </td>    
+                        </tr>
+                        <tr>
+                            <td colSpan={2}>
+                                <button type="button" id="ProcessButton">Process Payment</button>
+                            </td>
+                        </tr>
+                    </tbody> 
+                </table>
+            </form>
+        </div>
     </div>
 </template>
   
 <script setup>
-    onMounted(() => {
+    import { uuid } from 'vue3-uuid';
+    onMounted( async () => {
         // Retrieve Cardholder Name input - it will be provided to init() to copy its style to the MiraPayCapture fields 
         // var fieldStyle = $('input[name="CardholderName"]');
         var fieldStyle = document.getElementById('CardholderName');
@@ -61,11 +67,12 @@
         frameParentMap[MiraPayCapture.FRAME_TYPE_CVV] = "CVVCell";
 
         var form = document.getElementById("PaymentForm");
-        // TODO: Get the MPXSessionID from the procedure-create serverless functions
-        var MPXSessionID = document.getElementById("MPXSessionID").value;
-        // var MPXSessionID = 'aabbccdd-0123-4567-89ef-aabbccddeeff';
+        var id = uuid.v4();
 
-        // Initialize MiraPay Capture, while setting field styles 
+        const { pending, data: mpx } = await useAsyncData('mpx', () => $fetch('/.netlify/functions/procedure-create?id=' + id, { server: false}))
+        
+        // Initialize MiraPay Capture, while setting field styles
+        let MPXSessionID = mpx._rawValue.SDFields.SessionID 
         MiraPayCapture.init({
             sessionID: MPXSessionID, 
             frameParentMap: frameParentMap, 
@@ -77,11 +84,12 @@
                     form.style.display = 'block'
                 } else {
                     console.log('MiraPay Capture Init Failed');
-                form.style.display = 'block'
+                    form.style.display = 'block' // originally set to none to hide the form when MiraPay fails 
                 }
             } 
-        });
+        });     
     })
+        
 </script>
 
 <style scoped>
